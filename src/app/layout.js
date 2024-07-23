721daeceaa1c457d051ba8360c8df33f01e4f8d1
ArmAlFha <khwanchaidaugkaen@gmail.com>
2024-07-23 14:34:50 +0700

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,10 @@
-import { Inter } from "next/font/google";
+import {Prompt}from "next/font/google";
 import "./globals.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "./components/navbar";
+import BootstrapClient from "./components/BootstrapClient";
 
-const inter = Inter({ subsets: ["latin"] });
+const prompt = Prompt({ weight:['400', '700'], subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={prompt.className}><Navbar />{children}<BootstrapClient /></body>
     </html>
   );
 }
